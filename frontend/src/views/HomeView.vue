29d9e38dcb7599355a6c6fb660b479cf4d51dcd5
jeongwoogithub <leejeongpop@gmail.com>
2023-03-21 20:34:51 +0900

    feat: [이정우] 홈 화면 팝업창UI 생성[SSS-UI-9]

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,12 +1,15 @@
 <template>
-
+  <popup-form/>
 </template>
 
 <script>
 
-
+import PopupForm from "@/components/main/PopupForm.vue";
 export default {
   name: 'HomeView',
+  components: {
+    PopupForm
+  },
 
 }
 </script>
