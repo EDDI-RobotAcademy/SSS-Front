1904f6eab28aec3bb09ab61d2ca77cf92b5d7e95
kchangju <125858634+kchangju@users.noreply.github.com>
2023-03-21 14:01:10 +0900

    feat: [김창주] FAQ 게시판 생성 [SSS-PROCESS-8]
    feat: [김창주] FAQ 게시판 생성 [SSS-PROCESS-8]

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
+import JpaQnaBoardListPage from "@/views/lecture/board/JpaQnaBoardListPage.vue"
+import JpaQnaBoardRegisterPage from "@/views/lecture/board/JpaQnaBoardRegisterPage.vue"
+import JpaQnaBoardReadPage from "@/views/lecture/board/JpaQnaBoardReadPage.vue"
+import JpaQnaBoardModifyPage from "@/views/lecture/board/JpaQnaBoardModifyPage.vue"
+
 Vue.use(VueRouter)
 
 const routes = [
@@ -9,6 +14,36 @@ const routes = [
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/board-list-page',
+    name: 'JpaQnaBoardListPage',
+    component: JpaQnaBoardListPage
+  },
+  {
+    path: '/board-register',
+    name: 'JpaQnaBoardRegisterPage',
+    component: JpaQnaBoardRegisterPage
+  },
+  {
+    path: '/board-read/:boardId',
+    name: 'JpaQnaBoardReadPage',
+    components: {
+      default: JpaQnaBoardReadPage
+    },
+    props: {
+      default: true
+    }
+  },
+  {
+    path: '/board-modify/:boardId',
+    name: 'JpaQnaBoardModifyPage',
+    components: {
+      default: JpaQnaBoardModifyPage
+    },
+    props: {
+      default: true
+    }
   },
 
 ]
