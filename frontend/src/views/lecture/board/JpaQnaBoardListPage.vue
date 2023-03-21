ca3a1eee4841350435f081853ef691e3156a80df
kchangju <125858634+kchangju@users.noreply.github.com>
2023-03-21 20:26:15 +0900

    fix: [김창주] FAQ 게시물 리스트 페이지[SSS-UI-12]

--- frontend/src/views/lecture/board/JpaQnaBoardListPage.vue
+++ frontend/src/views/lecture/board/JpaQnaBoardListPage.vue
@@ -2,7 +2,7 @@
     <v-container>
       <h2>가장 자주하는 질문 FAQ</h2>
       <router-link :to="{ name: 'JpaQnaBoardRegisterPage' }">
-        게시물 작성
+         게시물 작성
       </router-link>
       <jpa-qna-board-list :boards="boards"/>
     </v-container>
