6e4b7711a4975998c4bb6284663c262e7a126fdd
jeongwoogithub <leejeongpop@gmail.com>
2023-03-22 16:15:31 +0900

    fix: [이정우] 팝업창UI 이미지 추가 [SSS-UI-9]

--- frontend/src/components/main/PopupForm.vue
+++ frontend/src/components/main/PopupForm.vue
@@ -7,47 +7,44 @@
           <v-icon>mdi-close</v-icon>
         </button>
       </div>
-      <v-img src="@/assets/logo.png">
-      </v-img>
-      <p>샐러드 설명</p>
-      <p>샐러드 설명</p>
-      <p>샐러드 설명</p>
-      <router-link to="/member/sign-up">
-        <v-btn>회원가입 하러...</v-btn>
-      </router-link>
-      <v-btn>이벤트 자세히...</v-btn>
+    <v-img src="@/assets/popup/SSS.jpeg" style="max-width: 300px; max-height: 180px;">
+    </v-img>
+    <p>샐러드 설명</p>
+    <p>샐러드 설명</p>
+    <p>샐러드 설명</p>
+    <router-link to="/member/sign-up">
+      <v-btn>회원가입 하러...</v-btn>
+    </router-link>
+    <v-btn>이벤트 자세히...</v-btn>
     </div>
   </v-container>
-
 </template>
 
 <script>
-
-
 export default {
-  name: 'HomeView',
+	name: 'PopupForm',
   data() {
     return {
-      title: '팝업 창 입니다.',
+      name: '팝업 창 입니다.',
       popup: true,
     }
   },
-
 }
 </script>
 
 <style scoped>
 .popup {
-  background-color: rgb(219, 237, 228);
+  background-color: rgb(245, 250, 248);
   position: fixed;
-  top: 200px;
-  right: 55px;
+  top: 280px;
+  right: 20px;
   padding: 20px;
   border-radius: 5px;
   box-shadow: 0px 0px 5px;
   z-index: 9999;
 
 }
+
 .close-btn {
   position: absolute;
   top: 10px;
@@ -56,5 +53,4 @@ export default {
   background-color: transparent;
   cursor: pointer;
 }
-
 </style>
