f6abfc6074b65d22c41613323ba9520680480d1c
jeongwoogithub <leejeongpop@gmail.com>
2023-03-23 06:25:40 +0900

    feat: [이정우] 최상단으로 갈 수 있는 top버튼 생성 [SSS-UI-14]

---
+++ frontend/src/components/main/TopButtonForm.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-container>
+  <a href="#" class="go-to-top">TOP</a>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'TopButtonPage',
+
+}
+</script>
+
+<style scoped>
+* {
+  margin:0;
+  padding:0;
+  font-size: 12px;
+}
+a.go-to-top{
+  position:absolute;
+  bottom:100px;
+  right:100px;
+  z-index:99;
+
+  display: inline-block;
+  width:30px;
+  height:30px;
+  border-radius: 15px;
+
+  line-height:30px;
+  text-align:center;
+  text-decoration:none;
+
+  background: #ccc;
+  opacity: 0.7;
+  color:#fff;
+}
+
+</style>
