55265d5002e986c1fd0fec711c4faa13f93e1e75
ParkHyeonMin <backend.bmo@gmail.com>
2023-03-27 07:35:10 +0900

    feat: [박현민] 마이페이지 UI [SSS-UI-19]
    마이페이지 구성 추가
    라우터 연결
    (닉네임)님 환영합니다 멘트 추가
    header 수정 << 로그인시 마이페이지도 보이게

---
+++ frontend/src/components/Acount/MyPage/MyPageViewForm.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="profile-section">
+        <!--마이페이지 css설정을 위한 div name 지정 my-page-box-->
+        <p>{{ memberInfoAboutSignIn.userNickName }}님 환영합니다</p>
+    <div class="my-page-box">
+        <!--아이디와 이메일주소 누구누구님환영합니다 비슷한거!-->
+        <div>
+            <p class="name head-text"></p>
+            <p class="email"></p>
+        </div>
+        <!--기본목록-->
+        
+    </div>
+</div>
+</template>
+
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+    props: {
+    nickname: String
+  },
+
+  computed: {
+    ...mapState(['memberInfoAboutSignIn']) 
+    
+  }
+
+}
+</script>
+
+<style>
+
+</style>
