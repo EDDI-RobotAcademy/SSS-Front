b64a197650058b288f17b39e89a0a21b567a38ea
jeongwoogithub <leejeongpop@gmail.com>
2023-04-05 15:56:48 +0900

    fix: [이정우]사이드메뉴 리스트 card컴포넌트 사용[SSS-UI-20]

--- frontend/src/components/SideProduct/SideProductListForm.vue
+++ frontend/src/components/SideProduct/SideProductListForm.vue
@@ -2,39 +2,26 @@
   <v-container>
     <v-row>
       <v-col v-for="(sideproduct, idx) in sideproducts" :key="idx" cols="3">
-        <v-img :src="require(`@/assets/selfSalad/${sideproduct.sideProductImg.editedImg.split('/').pop()}`)" />
-        <p class="product-title">{{ sideproduct.title }}</p>
-        <p class="product-price">{{ sideproduct.price | numberFormat }} 원</p>
+        <side-product-card :sideproduct="sideproduct"/>
       </v-col>
     </v-row>
   </v-container>
 </template>
-  
+
 <script>
+import SideProductCard from '@/components/SideProduct/SideProductCard.vue';
+
 export default {
-  name: "SideProductReadForm",
+  name: "SideProductListForm",
+  components: { SideProductCard },
   props: {
     sideproducts: {
       type: Array
     }
   },
-  filters: {
-    // 상품 금액 천단위 콤마 찍기
-    numberFormat(val) {
-      return String(val).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    }
-  },
 }
 </script>
-  
+
 <style scoped>
 
-.product-title {
-  margin-top: 5px;
-  font-weight: bold;
-}
-
-.product-price {
-  color: green;
-}
 </style>
