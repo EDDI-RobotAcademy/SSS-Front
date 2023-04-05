ff41b2815d0f09330f97571114ede07edc0440a6
jeongwoogithub <leejeongpop@gmail.com>
2023-04-05 16:03:39 +0900

    fix: [이정우] 사이드메뉴 상품 이미지 수정 [SSS-UI-23]

--- frontend/src/components/SideProduct/SideProductModifyForm.vue
+++ frontend/src/components/SideProduct/SideProductModifyForm.vue
@@ -1,97 +1,118 @@
 <template>
-    <v-container>
+  <v-container>
+    <form @submit.prevent="onSubmit" enctype="multipart/form-data" method="post">
+        <v-row>
+        <v-col cols="6">
+          <table>
+            <tr>
+              <td>게시물 번호</td>
+              <td>
+                <input type="text" :value="sideproduct.sideproductId" disabled/>
+              </td>
+            </tr>
+            <tr>
+              <td>메뉴무엇?</td>
+              <td>
+                <input type="text" v-model="title" />
+              </td>
+            </tr>
+            <tr>
+              <td>얼마야</td>
+              <td>
+                <input type="text" v-model.number="price" />
+              </td>
+            </tr>
+            <tr>
+              <td>본문</td>
+              <td>
+                <textarea cols="50" rows="20" v-model="content" />
+              </td>
+            </tr>
+            <tr>
+              <td>사진 변경</td>
+              <td>
+                <input type="file" id="files" ref="files" @change="handleFileUpload($event)" />
+              </td>
+            </tr>
+          </table>
+        </v-col>
+        <v-col cols="6">
+          <v-img :src="require(`@/assets/selfSalad/${sideproduct.sideProductImg.editedImg}`)" />
+
+          <v-img v-if="fileSelected" :src="require(`@/assets/selfSalad/${sideproduct.sideProductImg.editedImg}`)" />
+          <v-img v-else :src="imgUrl" :alt="'Image'"></v-img>
+        </v-col>
         <div>
-            <form @submit.prevent="onSubmit">
-                <div class="div">
-                    <table>
-                        <tr>
-                            <td>게시물 번호</td>
-                            <td>
-                                <input type="text" :value="sideproduct.productId" disabled />
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>메뉴무엇?</td>
-                            <td>
-                                <input type="text" v-model="title" />
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>얼마야</td>
-                            <td>
-                                <input type="text" v-model="price"/>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>본문</td>
-                            <td>
-                                <textarea cols="50" rows="20" v-model="content" />
-                            </td>
-                        </tr>
-                    </table>
-                </div>
-                <div>
-                    <v-btn type="submit" color="blue">수정 완료</v-btn>
-                    <router-link :to="{
-                        name: 'SideProductReadPage',
-                        params: { productId: sideproduct.productId.toString() }
-                    }">
-                        <v-btn color="red">취소</v-btn>
-                    </router-link>
-                </div>
-            </form>
+          <v-btn type="submit" color="blue">수정 완료</v-btn>
+          <router-link :to="{
+            name: 'SideProductReadPage',
+            params: { sideProductId: sideproduct.sideProductId }
+          }">
+            <v-btn color="red">취소</v-btn>
+          </router-link>
         </div>
-    </v-container>
+      </v-row>
+      </form>
+  </v-container>
 </template>
 
 <script>
 export default {
-  name:"SideProductModifyForm",
-  data(){
-    return{
-      title:"",
-      content:"",
-      price:0
+  name: "SideProductModifyForm",
+  data() {
+    return {
+      title: this.sideproduct.title,
+      content: this.sideproduct.content,
+      price: this.sideproduct.price,
+      imgUrl: "",
+      fileSelected: false,
     }
   },
-  props:{
-    sideproduct:{
+  props: {
+    sideproduct: {
       type: Object,
       required: true,
     }
   },
-  created() {
-    this.title = this.sideproduct.title
-    this.content = this.sideproduct.content
-    this.price = this.sideproduct.price
-  },
-  methods:{
-    onSubmit(){
-      const {title, content, price} = this
-      this.$emit('submit', {title, content, price})
+  methods: {
+    onSubmit() {
+      let formData = new FormData()
+      formData.append("fileList", this.files[0]);
+      const { sideProductId, title, content, price } = this
+      let productInfo = {
+        title: title,
+        content: content,
+        price: price,
+      }
+      formData.append(
+                  "productInfo",
+                  new Blob([JSON.stringify(productInfo)], { type: "application/json" })
+              )
+              for(let value of formData.values()){
+                console.log(value)
+              }
+              console.log(this.formData)
+              console.log(productInfo)
+
+              this.$emit('submit', { sideProductId, formData })
+          },
+    handleFileUpload(event) {
+      this.files = event.target.files;
+      this.imgUrl = URL.createObjectURL(this.files[0]);
     }
   }
 }
 </script>
 
 <style scoped>
-a {
-    text-decoration: none;
-}
-
-.div {
-    display: flex;
-    justify-content: center;
-}
 table {
-    border-collapse: collapse;
-    border: 2px solid black;
-    margin-bottom: 5px;
-  }
-  
-  td {
-    padding: 5px;
-    border: 2px solid black;
-  }
+  border-collapse: collapse;
+  border: 2px solid black;
+  margin-bottom: 5px;
+}
 
+td {
+  padding: 5px;
+  border: 2px solid black;
+}
 </style>
--- frontend/src/views/SideProduct/SideProductModifyPage.vue
+++ frontend/src/views/SideProduct/SideProductModifyPage.vue
@@ -1,51 +1,47 @@
 <template>
   <v-container>
     <div align="center">
-    <P><mark>등록한 상품을 수정하는 페이지 입니다.</mark></P>
-    <side-product-modify-form :sideproduct="sideproduct" @submit="onSubmit"/>
+      <P><mark>등록한 상품을 수정하는 페이지 입니다.</mark></P>
+      <side-product-modify-form :sideproduct="sideproduct" @submit="onSubmit" />
     </div>
   </v-container>
-  
 </template>
 
 <script>
 import SideProductModifyForm from '@/components/SideProduct/SideProductModifyForm.vue'
-import {mapActions, mapState} from 'vuex'
+import { mapActions, mapState } from 'vuex'
 export default {
-  components:{SideProductModifyForm},
+  components: { SideProductModifyForm },
   name: "SideProductModifyPage",
-  props:{
-    productId:{
+  props: {
+    sideProductId: {
       type: String,
       requires: true,
     }
   },
-computed: {
-  ...mapState(['sideproduct'])
-},
-methods:{
-  ...mapActions([
-    'requestSideProductToSpring',
-    'requestSideProductModifyToSpring'
-  ]),
-  async onSubmit(payload){
-    console.log("submit")
-    const { title, content, price } = payload
-    const productId = this.productId
-    await this.requestSideProductModifyToSpring({productId, title, content, price})
-    await this.$router.push({
-      name: 'SideProductReadPage',
-      params: { productId: this.productId.toString() }
-    })
+  computed: {
+    ...mapState(['sideproduct'])
+  },
+  methods: {
+    ...mapActions([
+      'requestSideProductToSpring',
+      'requestSideProductModifyToSpring',
+    ]),
+    async onSubmit(payload) {
+      const { title, content, price } = payload
+      const sideProductId = this.sideProductId
+      await this.requestSideProductModifyToSpring({ sideProductId, title, content, price })
+      await this.$router.push({
+        name: 'SideProductReadPage',
+        params: { sideProductId: this.sideProductId.toString() }
+      })
+    }
+  },
+  created() {
+    this.requestSideProductToSpring(this.sideProductId)
   }
-},
-created(){
-  this.requestSideProductToSpring(this.productId)
-}
-  
+
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
