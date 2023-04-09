5d302220659c458ad14bcdea7ea5c841c16f399c
jeongwoogithub <leejeongpop@gmail.com>
2023-04-09 18:16:54 +0900

    feat: [이정우] 사이드 네비 바 컴포넌트 이벤트 연결[SSS-UI-37]

--- src/views/NavigationMenu/Header.vue
+++ src/views/NavigationMenu/Header.vue
@@ -1,8 +1,8 @@
 <template>
   <nav>
-    <v-app-bar class="flex-grow-0" color="white">
+    <v-app-bar class="flex-grow-0" app color="white">
       <ul class="menu mt-3">
-          <li class="logo">
+        <li class="logo">
             <router-link :to="{ name: 'home' }">
               <img src="@/assets/logo/3sss-2.jpg" width="100">
             </router-link>
@@ -23,11 +23,11 @@
             <router-link :to="{ name: 'IngredientListPage' }">Self Salad</router-link>
           </li>
         </ul>
-
-      <v-spacer></v-spacer>
-      <template v-if="!this.$store.state.signInValue">
-        <v-btn>
-          <router-link :to="{ name: 'SignUp' }">signUp</router-link>
+        
+        <v-spacer></v-spacer>
+        <template v-if="!this.$store.state.signInValue">
+          <v-btn>
+            <router-link :to="{ name: 'SignUp' }">signUp</router-link>
           <v-icon right>mdi-account-plus</v-icon>
         </v-btn>  
         <v-btn>
@@ -45,18 +45,48 @@
             <v-icon right>mdi-history</v-icon>
           </v-btn>
         </template>
-
-        <!--마이페이지 링크-->
+            <v-app-bar-nav-icon @click="navigation_drawer = !navigation_drawer"/>
     </v-app-bar>
+    <!-- 컴포넌트 보내기 전 연습 해보던거 -->
+    <!-- <v-navigation-drawer app v-model="navigation_drawer" right>
+      <v-list-item >
+        <v-list-item-content>
+          <div class="div">
+        </div>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list nav dense>
+        <v-list-item v-for="link in links" :key="link.name" router :to="link.route">
+          <v-list-item-action>
+            <v-icon left>
+              {{ link.icon }}
+            </v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>
+              {{ link.text }}
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer> -->
+    <side-nav/>
   </nav>
   
 </template>
 
 <script>
 import { mapState } from 'vuex';
+import SideNav from '@/views/NavigationMenu/SideNav.vue'
 
 export default {
   name: "Header",
+  components: { SideNav },
+  data() {
+    return {
+      navigation_drawer: true
+    }
+  },
   methods: {
     ...mapState([
       'signInValue'
---
+++ src/views/NavigationMenu/SideNav.vue
@@ -0,0 +1,82 @@
+<template>
+  <v-container style="width: 1400px">
+    <v-navigation-drawer height="100%" width="260px" app color="#367a2c" v-model="navigation_drawer" right>
+      <v-list-item style="height: 25%">
+        <v-list-item-content>
+          <v-list-item-title class="mb-5" style="margin-top: 250px">
+            <div align="center">
+              <router-link to="/order-manage">
+                <v-avatar :size="120" color="grey lighten-4">
+                  <!-- <img src="@/assets/default_profile_image.png" alt="avatar"> -->
+                </v-avatar>
+              </router-link>
+            </div>
+          </v-list-item-title>
+
+          <v-list-item-subtitle class="white--text justify-center mt-8" style="font-size: 16px">
+            <div align="center">
+              {{ nickname }} 님 환영합니다!
+            </div>
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      <v-divider style="border-color: white; margin-top: 150px"></v-divider>
+
+
+      <v-list dense nav class="mt-3">
+        <v-list-item clas="tile" v-for="item in items" :key="item.title" :to="item.route" link class="mt-5 mb-5 ml-5">
+          <v-list-item-icon>
+            <v-icon color="white">{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content style="height: 100%">
+            <v-list-item-title class="white--text">
+              {{ item.title }}
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "BuyerNav",
+  data() {
+    return {
+      navigation_drawer: true,
+      nickname: 'sss',
+      items: [
+        {
+          title: "내 정보",
+          icon: "mdi-account",
+          route: "/"
+        },
+        {
+          title: "주문 배송",
+          icon: "mdi-currency-usd",
+          route: "/"
+        },
+        {
+          title: "나의 문의",
+          icon: "mdi-comment-question",
+          route: "/"
+        },
+        {
+          title: "나의 후기",
+          icon: "mdi-star",
+          route: "/"
+        },
+        {
+          title: "찜 했다",
+          icon: "mdi-lightbulb-on",
+          route: "/"
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style scoped></style>
