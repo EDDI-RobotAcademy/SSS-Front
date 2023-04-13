93c94a4effbdf9ebe93db3a268813e8798d6968c
kchangju <bmlfid519@gmail.com>
2023-04-13 21:29:47 +0900

    feat: [김창주] 사용자가 상품페이지에 등록된 상품 검색하는 UI [SSS-UI-46]

--- src/views/Product/ProductListPage.vue
+++ src/views/Product/ProductListPage.vue
@@ -5,7 +5,14 @@
         <router-link :to="{ name: 'ProductRegisterPage' }">
             상품 등록
         </router-link>
-        <product-list-form :products="products"/>
+        <div>
+          <select v-model="searchBy">
+            <option value="title">제품명</option>
+          </select>
+          <input type="text" v-model="searchQuery" placeholder="검색" @keyup.enter="searchProducts">
+          <button @click="searchProducts">검색</button>
+        </div>
+        <product-list-form :products="dpitems" :current-page="currentPage" />
       </v-row>  
     </v-container>
   </template>
@@ -21,18 +28,49 @@
   export default {
     components: { ProductListForm, ProductCard },
     name: "ProductListPage",
+    data () { 
+      return {
+        searchQuery: '',
+        dpitems: [],
+        searchBy: 'title',
+        currentPage: 1,
+      }
+    },
     computed:{
       ...mapState(productModule, [
         'products'
-      ])
+      ]),
     },
-    async mounted (){
+    async mounted () {
       await this.requestProductListToSpring()
     },
     methods:{
       ...mapActions(productModule, [
         'requestProductListToSpring'
-      ])
+      ]),
+      searchProducts(){
+        const query = this.searchQuery.toLowerCase();
+        if (query === '') {
+          this.dpitems = this.products;
+        } else {
+          if (this.searchBy === 'title') { 
+            this.dpitems = this.products.filter(product => product.title.toLowerCase().includes(query));
+          }
+        }
+        this.currentPage = 1;
+      }
+    },
+    created () {
+      if (this.products && this.products.length > 0) {
+        this.searchProducts();
+      }
+    },
+    watch: {
+      products() {
+        if (this.products && this.products.length > 0) {
+          this.searchProducts();
+        }
+      },
     }
   }
   </script>
