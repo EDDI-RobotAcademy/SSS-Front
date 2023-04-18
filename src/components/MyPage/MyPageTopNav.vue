a278834ed5f29bda17dc9e3034c0857c7e9a9086
jeongwoogithub <leejeongpop@gmail.com>
2023-04-18 15:08:10 +0900

    fix:[이정우] 로그인한 사용자가 볼 수 있는 마이페이지에 카테고리 카운트 네비 바 수정[SSS-UI-48]

--- src/components/MyPage/MyPageTopNav.vue
+++ src/components/MyPage/MyPageTopNav.vue
@@ -1,33 +1,35 @@
 <template>
-  <v-container style="width: 1400px; height: 300px; margin-right: 100px;">
-    <v-card style="height: 150px; margin-top: 80px;" elevation="0" tile fla>
-      <v-row>
-      <v-col v-for="(item, index) in myPageCategoryItems"
-             :key="index">
-          <v-hover
-              v-slot="{ hover } "
-          >
-              <v-card class="cardStyle"
-                      :elevation="hover ? 20 : 0"
-                      :class="{'on-hover' : hover}"
-                      tile
-                      flat
-              >
-                <v-card-title class="justify-center">
-                  <h5>{{ item.name }}</h5>
-                </v-card-title>
-
-                <v-card-title class="justify-sm-end" style="margin-right: 10px">
-                    <h5 style="font-size: 33px; color: #FAEBD7">{{item.count}}</h5>
-                  <span style="margin-top: 7px">
-                  개
-                </span>
-                </v-card-title>
-              </v-card>
-          </v-hover>
+  <v-container>
+    <div style="height: 140px;">
+      <div>
+      <div align="left" style="font-weight: bold; font-size: 24px;">
+        {{ nickname }} 님 환영합니다!
+      </div>
+      <div align="right">
+        <router-link to="/my-info-account" class="ifno-black">
+          <span>내 정보 관리</span>
+        </router-link>
+      </div>    
+    </div>
+    <v-row>
+      <v-col v-for="(item, index) in myPageCategoryItems" :key="index">
+        <v-card :to="item.route" class="d-flex" @click="$router.push(item.route)"
+        :class="{ 'selected': selected === index }"
+      >
+          <v-card-title class="text-start">
+            <v-icon>{{ item.icon }}</v-icon>
+            <h4>{{ item.title }}</h4>
+          </v-card-title>
+          <v-card-title class="text-end">
+            <h5 style="font-size: 33px; color: #e8d7c1">{{item.count}}</h5>
+            <span>개</span>
+          </v-card-title>
+        </v-card>
       </v-col>
     </v-row>
-    </v-card>
+      
+
+  </div>
   </v-container>
 </template>
 
@@ -37,26 +39,53 @@ export default {
   name: "MyPageTopNav",
   data() {
     return {
-      //예시
+      selected: null,
+      nickname: 'sss',
       myPageCategoryItems: [
-        { name: "장바구니", count: 0 },
-        { name: "찜", count: 33 },  
-        { name: "나의 후기", count: 4 },
-        { name: "나의 문의", count: 12 },
+        {
+          title: "장바구니",
+          icon: "mdi-cart-variant",
+          route: "/my-info-cart",
+          count: 8
+        },
+        {
+          title: "찜찜",
+          icon: "mdi-star-circle",
+          route: "/my-info-favorite",
+          count: 15
+        },
+        {
+          title: "리뷰",
+          icon: "mdi-fountain-pen-tip",
+          route: "/my-info-review",
+          count: 2
+        },
+        {
+          title: "문의 사항",
+          icon: "mdi-human-greeting",
+          route: "/my-info-Qna",
+          count: 1
+        }
       ]
+    }
+  },
+  methods: {
+    handleClick(index) {
+      this.selected = index;
     }
   }
 }
 </script>
 
 <style scoped>
-.cardStyle {
-  height: 150px;
-  width: 215px;
-  padding: 10px 10px;
-  background-color: #80a84f;
-  color: white;
-  border-right: 1px solid white;
-  border-left: 1px solid white;
+  .ifno-black {
+    text-decoration: none;
+    color: black;
+    font-weight: bold
+  }
+.selected {
+  border: 2px solid #e8d7c1;
+  background-color: #e8d7c1;
+  color: #fff;
 }
 </style>
