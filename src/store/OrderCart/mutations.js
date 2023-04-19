a6af8a9f64c35a3a7326b4c806633c7c9a947789
jeongwoogithub <leejeongpop@gmail.com>
2023-04-20 00:03:31 +0900

    add:[이정우] 사용자가 장바구니에 담은 셀프샐러드를 수정할 수 있는 재료 리스트 생성 [SSS-UI-59]

--- src/store/OrderCart/mutations.js
+++ src/store/OrderCart/mutations.js
@@ -1,6 +1,7 @@
 import {
   REQUEST_ADD_CART_LIST_TO_SPRING,
-  REQUEST_SELFSALAD_ADD_CART_LIST_TO_SPRING
+  REQUEST_SELFSALAD_ADD_CART_LIST_TO_SPRING,
+  REQUEST_SELFSALAD_TO_SPRING
 } from './mutation-types'
 
 export default {
@@ -9,5 +10,8 @@ export default {
   },
   [REQUEST_SELFSALAD_ADD_CART_LIST_TO_SPRING] (state, passingData) {
       state.cartItems = passingData
+  },
+  [REQUEST_SELFSALAD_TO_SPRING] (state, passingData) {
+      state.cartItem = passingData
   }
 }
