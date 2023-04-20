e95f44b8e25ea6de9f7ff747156d12bfa6a50ca1
kchangju <bmlfid519@gmail.com>
2023-04-20 15:51:14 +0900

    feat: [김창주] 사용자가 게시판에 비밀글을 작성하는 UI [SSS-UI-44]

--- src/components/Qnaboard/QnaBoardRegisterForm.vue
+++ src/components/Qnaboard/QnaBoardRegisterForm.vue
@@ -10,7 +10,7 @@
       <tr>
         <td>작성자</td>
         <td>
-          <input placeholder="작성자 이름을 입력하세요." type="text" v-model="writer"/>
+          {{this.$store.state.memberModule.memberInfoAboutSignIn.userNickName}}
         </td>
       </tr>
       <tr>
@@ -20,6 +20,14 @@
         </td>
       </tr>
     </table>
+    <div align="center">
+      <v-checkbox
+      @click="click"
+        label="비밀글 문의"
+        color="#205C37"
+        v-model="privateCheck">
+      </v-checkbox>
+    </div>
     <div class="bottom">
       <button type="submit">등록</button>
       <router-link to="{ name: 'QnaBoardListPage' }">
@@ -36,35 +44,118 @@ export default {
     data () {
         return {
           title: '',
-          writer: '',
           content: '',
+          writer: '',
+          memberId: '',
+          privateCheck: false,
         }
     },
+    computed: {
+    },
+    created() {
+      this.writer = this.$store.state.memberModule.memberInfoAboutSignIn.userNickName;
+      this.memberId = this.$store.state.memberModule.memberInfoAboutSignIn.userId;
+    },
     methods: {
+      click(){
+        console.log("@@@@@")
+        console.log("@@@@@"+this.privateCheck)
+      },
         onSubmit () {
-            const { title, writer, content } = this
-            this.$emit('submit', { title, writer, content })
-        }
+            if(this.$store.state.memberModule.isAuthenticated == true) {
+                if (this.title=='') {
+                alert('제목을 입력해주세요')
+              } else if (this.content=='') {
+                alert('내용을 작성해주세요')
+              } else {
+                const { title, content, privateCheck } = this
+                const writer = this.$store.state.memberModule.memberInfoAboutSignIn.userNickName;
+                const memberId = this.$store.state.memberModule.memberInfoAboutSignIn.userId;
+                this.$emit('submit', { title, writer, content, privateCheck, memberId })
+              }
+            } else {
+              alert("로그인이 필요한 기능입니다.")
+              this.$router.push('/member/sign-in')
+            }
+        },
     }
-}
+  }
+  
 
 </script>
 
 <style scoped>
 table {
-  margin-left: auto;
-  margin-right: auto;
+  margin: 0 auto;
   border-collapse: collapse;
   border: 1px solid black;
-  margin-bottom: 5px;
+  margin-bottom: 10px;
+  background-color: white;
 }
 
 td {
-  padding: 5px;
+  padding: 10px;
   border: 2px solid black;
 }
+
+input[type="text"], textarea {
+  width: 100%;
+  border: none;
+  padding: 10px;
+  margin-bottom: 0px;
+  font-size: 16px;
+  background-color: white;
+}
+
 .bottom {
+  margin-top: 20px;
   text-align: center;
+}
+
+button {
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  background-color: #205C37;
+  color: white;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: #3D8745;
+}
+
+v-checkbox {
+  margin-top: 10px;
+}
+
+label {
+  color: #205C37;
+  font-weight: bold;
+}
+
+textarea {
+  resize: none;
+  height: 300px;
+}
+
+h1 {
+  text-align: center;
+  font-size: 24px;
+  margin-bottom: 30px;
+  color: #205C37;
+}
+
+.alert {
+  margin-top: 20px;
+  padding: 10px;
+  background-color: #f44336;
+  color: white;
+  text-align: center;
+}
+
+.privateCheck {
+  color: #205C37;
 }
 
 
