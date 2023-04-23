05cd44f7cbf50f6cf282dca4c72afc95ff962223
dduru-nuna <syj00062@gmail.com>
2023-04-23 20:32:30 +0900

    feat: [서윤지] 회원의 찜 목록에서 장바구니에 담을 수 있는 기능 [SSS-UI-47]

--- src/views/MyPage/MyFavoritePage.vue
+++ src/views/MyPage/MyFavoritePage.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container>
     <my-page/>
-    <my-favorite-form/>
+    <my-favorite-form @addCart="addCart"/>
   </v-container>
 </template>
 
@@ -10,11 +10,29 @@
 
 import MyFavoriteForm from "@/components/MyPage/MyFavoriteForm.vue";
 import MyPage from "@/views/MyPage/MyPage.vue";
+import { mapActions } from 'vuex'
 
+const ordercartModule = 'ordercartModule'
 
 export default {
   name: "MyFavoritePage",
-  components: { MyFavoriteForm, MyPage }
+  components: { MyFavoriteForm, MyPage },
+  methods: {
+    ...mapActions(ordercartModule, [        
+          'requestAddCartToSpring'    
+        ]),
+    async addCart(payload) {
+            const memberId = this.$store.state.memberModule.memberInfoAboutSignIn.userId
+            const itemId = payload.productId
+            const quantity = payload.quantity
+            const category = 'PRODUCT'
+            console.log('memberId: '+ memberId )
+            console.log('productId: '+ itemId )
+            console.log('quantity: '+ quantity )
+            console.log('category: '+ category )
+            await this.requestAddCartToSpring({memberId, itemId, category, quantity})
+          },
+  }
 }
 </script>
 
