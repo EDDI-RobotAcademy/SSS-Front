4fd08df62aaa83c6c8e23a472ebbe5bbae8fb2ae
ParkHyeonMin <backend.bmo@gmail.com>
2023-04-23 09:30:56 +0900

    fix: [박현민] 회사소개 게시판 UI [SSS-UI-9]
    회사 소개 페이지 전체 수정

--- src/views/IntroductionPage.vue
+++ src/views/IntroductionPage.vue
@@ -1,81 +1,93 @@
 <template>
-    <container>
-        <div class="img-text">
-            <!--로고 이미지-->
-            <header>
-                <h1 style="text-align: center;"><img src="@/assets/logo/3sss-ko.jpg" width="500"></h1>
+    <v-container>
+        <v-row justify="center">
+      <div class="row">
+        <header style="text-align: center;">
+            <v-col>
+                <img src="@/assets/logo/3sss-ko.jpg" style="max-width: 600px; min-width: 300px; width: 100%;">
+                <div style="font-size: 35px; padding: 10px;"> 당신의 건강한 하루를 지금 바로 시작하세요.</div>
+            </v-col>
             </header>
-            <!--글 내용-->
-            <body>
-                <div>
-                <h3 style="text-align: center; margin-block:30px;"> 당신의 건강한 하루를 지금 바로 시작하세요.</h3>
-
-                <h1 class="mainImage" style="text-align: center;">
-                    <video class="rounded-3" autoplay="autoplay" loop="loop" muted="muted" style="height: 800px;">
-                    <source src="@/assets/Introduction/SSS3.mp4" type="video/mp4">
-                    </video>
-                </h1>
+            <div class="longline"/>
+        <!-- 좌측 회사 소개 내용 -->
+        <div class="col-md-6">
+          <div class="company-info">
+            <div class="logo">
+              <img src="@/assets/Introduction/SSSIntroduction2.jpg" style="height: inherit;: 500px;" alt="company-logo">
             </div>
-            </body>
-        </div>
-
-        <!--회사 소개의 글-->
-        <div style="text-align: center; margin-block:30px;">
-            <h1>Self Salad Sell</h1>
-            <p>저희는 신선하고 좋은 상품만 취급합니다.<br>
-            SSS는 시중에서 쉽게 접할 수 없는 멀티리프 계열의 유럽형 채소를 직접 재배함으로써 메뉴의 맛과 식감을 극대화하고 있습니다.<br>
-            특히 최근 도입한 수경재배방식(NFT)은 공급의 안정성은 물론 원물의 신선도를 한층 높이는데 기여하고 있습니다.</p>
-        </div>
-
-        <div style="text-align: center; margin-block:30px;">
-            <span><img class="border-bottom rounded-3" src="@/assets/popup/SSS.jpeg" style="max-width: 300px; max-height: 300px; margin: 10px;"></span>
-            <span><img class="border-bottom rounded-3" src="@/assets/popup/SSS.jpeg" style="max-width: 300px; max-height: 300px; margin: 10px;"></span>
-            <span><img class="border-bottom rounded-3" src="@/assets/popup/SSS.jpeg" style="max-width: 300px; max-height: 300px; margin: 10px;"></span>    
-        </div>
-
-        <div style="background-color: #f6f6f6; padding: 10px;">   
-            <h3 style="text-align: center; margin-block:30px;"> 찾아오시는 길</h3>
-        <!-- 중앙정렬 div태그 안에 만들어서 넣어줄 것 -->
-        <div style= "display: flex; justify-content: center; align-items: center;">
-            <!-- 지도의 크기 -->
-            <div class="rounded-3" id="map" style ="width:800px; height:400px;"></div>
-        </div>
-        
-        <div style= "display: flex; justify-content: center; align-items: center;">
-            <div>
-                <hr>
-                <table class="table">
-                <table class="table table-striped table-bordered table-hover">
-                <t-body>
-                    
-                    <tr>
-                        <td>주소</td>
-                        <td>
-                            서울특별시 강남구 테헤란로 14길 6 남도빌딩 2F, 3F, 4F, 5F, 6F (T: 1544-9970 / F: 02-562-2378)
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>버스</td>
-                        <td>
-                             역삼역.포스코P&S타워 정류장
-                            지선 146 / 740 / 341 / 360         간선 1100 / 1700 / 2000 / 7007 / 8001
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>지하철</td>
-                        <td>
-                            지하철 2호선 역삼역 3번출구 100m
-                        </td>
-                    </tr>
-                </t-body>
-                </table>
-            </table>
+            <div class="description" style="min-width: 330px;">
+                
+              <p>샐러드는 건강한 식습관을 추구하는 사람들을 위해 만들어졌습니다. SSS는 최상의 품질과 맛을 추구하며,
+                항상 신선한 재료와 정성으로 만든 메뉴를 제공합니다. 고객의 니즈에 따라 맞춤형 샐러드를 제공하고, 
+                항상 건강한 식습관을 유지할 수 있도록 노력합니다.</p>
+           
             </div>
+          </div>
         </div>
-    </div>
-    </container>
-</template>
+        <!-- 우측 이미지 -->
+        <div class="col-md-6">
+          <div class="image">
+            <img src="@/assets/Introduction/SSSIntroduction1.jpg" alt="company-image">
+          </div>
+        </div>
+      </div>
+      <div>
+        <v-row justify="center">
+      <v-col cols="3" class="text-center">
+        <v-icon class="icon" color="primary">mdi-food-apple</v-icon>
+        <div class="icon-title">샐러드 원료</div>
+        <div class="icon-text">우리는 항상 신선하고 건강한 채소와 과일을 고르며, 최상의 품질을 유지합니다. 샐러드를 먹는 첫 단계는 원료의 선택부터입니다.</div>
+      </v-col>
+      <v-col cols="3" class="text-center">
+        <v-icon class="icon" color="primary">mdi-seed</v-icon>
+        <div class="icon-title">신선한 재료</div>
+        <div class="icon-text">샐러드는 신선한 재료에서 비롯됩니다. 우리는 매일 아침 신선한 채소와 과일을 공급받으며, 신선함을 유지하기 위해 노력합니다.</div>
+      </v-col>
+      <v-col cols="3" class="text-center">
+        <v-icon class="icon" color="primary">mdi-knife</v-icon>
+        <div class="icon-title">정성 가공</div>
+        <div class="icon-text">우리는 손질과 가공에서도 최상의 품질을 유지합니다. 모든 채소와 과일은 신선한 상태에서 손질되며, 정성스럽게 가공됩니다.</div>
+      </v-col>
+      <v-col cols="3" class="text-center">
+        <v-icon class="icon" color="primary">mdi-bowl-mix</v-icon>
+        <div class="icon-title">고객 맞춤형</div>
+        <div class="icon-text">샐러드는 고객의 니즈에 맞춤형으로 제공됩니다. 우리는 고객의 취향에 맞춰 다양한 종류의 채소와 과일, 그리고 드레싱을 제공합니다.</div>
+      </v-col>
+    </v-row>
 
+  </div>
+  
+      <div class="col-md-12">
+        <div class="directions">
+          <h3 class="title">오시는 길</h3>
+          <div class="line"/>
+          <div class="map-container">
+            <div id="map"></div>
+          </div>
+          <div class="address">
+            <p class="title">주소</p>
+            <div class="line"/>
+            <p>서울특별시 강남구 테헤란로 14길 6 남도빌딩 2F, 3F, 4F, 5F, 6F</p>
+          </div>
+          <div class="bus">
+            <p class="title">버스</p>
+            <div class="line"/>
+            <ul>
+              <li>역삼역.포스코P&amp;S타워 정류장</li>
+              <li>지선 146 / 740 / 341 / 360</li>
+              <li>간선 1100 / 1700 / 2000 / 7007 / 8001</li>
+            </ul>
+          </div>
+          <div class="subway">
+            <p class="title">지하철</p>
+            <div class="line"/>
+            <p>지하철 2호선 역삼역 3번출구 100m</p>
+          </div>
+        </div>
+      </div>
+    </v-row>
+    </v-container>
+  </template>
 <script>
 
 export default {
@@ -144,34 +156,102 @@ export default {
 </script>
   
   
-<style>
-.img-text {
-  margin-top: 50px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+<style scoped>
+  .company-info {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    height: 100%;
+  }
+  .company-info .logo img {
+    width: 350px;
+    height: 120px;
+    margin-bottom: 40px;
+  }
+  .company-info .description h2 {
+    margin-top: 0;
+    font-size: 30px;
+    font-weight: bold;
+    text-align: center;
+  }
+  .company-info .description p {
+    font-size: 20px;
+    line-height: 1.5;
+    margin-bottom: 20px;
+    text-align: center;
+  }
+  .image img {
+    width: 100%;
+    height: auto;
+  }
+
+  .icon {
+  font-size: 70px;
+  margin: 0 10px;
+  cursor: pointer;
 }
-.map {
-  margin-top: 50px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+
+@media (min-width: 600px) {
+  .icon {
+    font-size: 90px;
+  }
+}
+
+@media (min-width: 960px) {
+  .icon {
+    font-size: 150px;
+  }
+
 }
 
 
-.mainImage {
-    filter: blur(1px);
+  .map-container {
+    position: relative;
+    width: 100%;
+     /* 지도의 높이를 설정할 수 있습니다 */
+  }
+  #map {
+    width: auto;
+    height: 400px;
+  }
+  .address, .bus, .subway {
+    margin-top: 20px;
+  }
+  .title {
+    font-weight: bold;
+    margin-bottom: 5px;
+  }
+  .titlemain {
+    font-size: 40px;
+    font-weight: bold;
+    margin-bottom: 5px;
+  }
+  * {
+    margin-bottom: 8px;
+    font-size: 15px;
+    color: #40513B;
+    }
+
+
+
+    .material-symbols-outlined {
+  font-variation-settings:
+  'FILL' 0,
+  'wght' 400,
+  'GRAD' 0,
+  'opsz' 48
 }
 
-#content .conTit:after {
-    display: block;
-    content: '';
-    width: 30px;
-    height: 3px;
-    background: #333;
-    position: absolute;
-    bottom: 0;
-    left: 50%;
-    margin-left: -15px;
+.line {
+    width: 33.3%;
+    border-bottom: 3px solid #40513B;
+    margin: 10px 0;
 }
+.longline{
+    width: 100%;
+    border-bottom: 3px solid #40513B;
+    margin: 10px 0;
+}
+
 </style>
