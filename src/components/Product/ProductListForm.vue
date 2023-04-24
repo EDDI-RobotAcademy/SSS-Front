17bf120d47b56058cfbe7d5b74921b1aad148bf9
dduru-nuna <syj00062@gmail.com>
2023-04-24 21:27:35 +0900

    feat: [서윤지] 완제품 목록 페이지네이션 [SSS-UI-5]

--- src/components/Product/ProductListForm.vue
+++ src/components/Product/ProductListForm.vue
@@ -4,10 +4,17 @@
       <v-col v-if="!products || (Array.isArray(products) && products.length === 0)">
         <p>현재 등록된 상품이 없습니다!</p>
       </v-col> 
-      <v-col style="min-width: 300px;" v-else v-for="product in products" :key="product.productId" cols="3">
+      <v-col style="min-width: 300px;" v-else v-for="(product, index) in calData" :key="index" cols="3">
         <product-card :product="product"></product-card>
       </v-col>
     </v-row>
+    <v-pagination
+      v-model="curPageNum"
+      :length="numOfPages"
+      color="#2c4636"
+      class="mt-10"
+      flat
+    ></v-pagination>
   </v-container>
   </template>
   <script>
@@ -18,11 +25,32 @@
         components: {
           ProductCard
         },
+        data() {
+          return {
+            listData: ["products"],
+            dataPerPage: 8,
+            curPageNum: 1,
+          };
+        },
         props: {
           products: {
               type: Array
           }
-        }
+        },
+        computed: {
+          startOffset() {
+            return (this.curPageNum - 1) * this.dataPerPage;
+          },
+          endOffset() {
+            return this.startOffset + this.dataPerPage;
+          },
+          numOfPages() {
+            return Math.ceil(this.products.length / this.dataPerPage);
+          },
+          calData() {
+            return this.products.slice(this.startOffset, this.endOffset);
+          },
+        },
     }
   </script>
   
