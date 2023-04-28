376e7b048f4e44d40d4d282e18a38975366d0c87
jeongwoogithub <leejeongpop@gmail.com>
2023-04-28 16:07:51 +0900

    fix:[이정우]사용자가 마이페이지에서 본인이 등록한 글을 확인 할 수 있는 기능 연결 [SSS-UI-49]

--- src/views/MyPage/MyQnaPage.vue
+++ src/views/MyPage/MyQnaPage.vue
@@ -1,21 +1,29 @@
 <template>
   <v-container>
     <my-page/>
-    <my-qna-form :boards="boardList" :current-page="currentPage"/>
+    <div class="outer">
+      <div class="inner">
+        <input type="text" v-model="searchQuery" placeholder="검색" @keyup.enter="searchBoards">
+        <button @click="searchBoards">검색</button>
+      </div>
+      <div class="list">
+        <my-qna-form :boards="boardList" :current-page="currentPage" />
+      </div>
+    </div>
   </v-container>
 </template>
 
 <script>
-import MyQnaForm from "@/components/MyPage/MyQnaForm.vue";
-import MyPage from "@/views/MyPage/MyPage.vue";
-import QnaBoardList from '@/components/Qnaboard/QnaBoardList.vue'
+import MyQnaForm from '@/components/MyPage/MyQnaForm.vue'
+import MyPage from "@/views/MyPage/MyPage.vue"
+
 import { mapActions, mapState } from 'vuex'
 
 const qnaModule = 'qnaModule'
 
 export default {
-  name: "MyInfoQnaPage",
   components: { MyQnaForm, MyPage },
+  name: "MyInfoQnaPage",
   data () { 
     return {
       searchQuery: '',
@@ -30,7 +38,35 @@ export default {
   mounted () {
     this.requestBoardListToSpring()
   },
+  created() {
+    //브라우저 새로고침 해도 게시물 목록 계속 보이게 수정
+    if (this.boards && this.boards.length > 0) {
+      this.searchBoards();
+    }
+  },
+  watch: {
+    boards() {
+      //브라우저 새로고침 해도 게시물 목록 계속 보이게 수정
+      // 게시물 목록이 변경될 때마다 searchBoards() 호출
+      if (this.boards && this.boards.length > 0) {
+        this.searchBoards();
+      }
+    }
+  },
   methods: {
+    searchBoards() {
+      const query = this.searchQuery.toLowerCase();
+      if (query === ''){
+        this.boardList = this.boards;
+      } else {
+        if (this.searchBy === 'title') {
+          this.boardList = this.boards.filter(board => board.title.toLowerCase().includes(query));
+        } else if (this.searchBy === 'writer') {
+          this.boardList = this.boards.filter(board=> board.writer.toLowerCase().includes(query));
+        }
+      }
+      this.currentPage = 1;
+    },
     ...mapActions(qnaModule, [
       'requestBoardListToSpring'
     ]),
@@ -40,4 +76,74 @@ export default {
 </script>
 
 <style scoped>
+
+.box {
+  text-align: center;
+  margin-bottom: 30px;
+}
+
+.center {
+  display: flex;
+  justify-content: right;
+  margin-bottom: 30px;
+}
+
+.center p {
+  color: #fff;
+  background-color: #739e42;
+  padding: 10px 15px;
+  border-radius: 5px;
+  font-weight: 500;
+  font-size: 15px;
+  cursor: pointer;
+  text-decoration: none;
+}
+
+.center p:hover {
+  background-color: #739e42;
+}
+
+.outer {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.inner {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 30px;
+}
+
+.inner select {
+  width: 100px;
+  height: 30px;
+  border-radius: 5px;
+  border: 1px solid #739e42;
+  margin-right: 10px;
+}
+
+.inner input[type='text'] {
+  width: 300px;
+  height: 30px;
+  border-radius: 5px;
+  border: 1px solid #739e42;
+  padding-left: 10px;
+  margin-right: 10px;
+}
+
+.inner button {
+  width: 70px;
+  height: 35px;
+  background-color: #739e42;
+  border: none;
+  border-radius: 5px;
+  color: #fff;
+  font-size: 16px;
+  cursor: pointer;
+}
+
+.inner button:hover {
+  background-color: #739e42;
+}
 </style>
