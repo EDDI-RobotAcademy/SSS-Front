8e35dba8cb6473f23de416c72208028ebabebf9f
jeongwoogithub <leejeongpop@gmail.com>
2023-04-29 21:43:44 +0900

    fix:[이정우] 홈 화면에서 보여질 비디오 자동 슬라이드 UI 하단 버튼 추가 및 오타 수정[SSS-UI-6]

--- src/components/main/CarouselForm.vue
+++ src/components/main/CarouselForm.vue
@@ -13,7 +13,7 @@
 							<div class="col-sm-12 col-md-6 text-md-start">
 								<div class="py-5" style="text-align: right; margin-right: 25px; min-width: 700px;" >
 									<h1 style="font-size: 80px;" class="text-white fw-bold">
-										Welcome to <br>Self Salade Sell
+										Welcome to <br>Self Salad Sell
 									</h1>
 									<p  class="text-white mb-4 fs-2">
 										당신의 샐러드로 건강한 하루를 시작하세요.
@@ -45,6 +45,10 @@
 					</div>
 				</div>
 			</div>
+		</div>
+		<div class="carousel-indicators">
+			<button type="button" data-bs-target="#mainTopCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+			<button type="button" data-bs-target="#mainTopCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
 		</div>
 		</div>
 		</div>
